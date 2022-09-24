d06a3ad54303fc0402f71f3dad9dfbb79bedf7b9
daver987 <daveyalexander@outlook.com>
2022-09-24 03:50:51 -0400

    Add: tailwindcss to template

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <main>
-    <h1>This is our main content</h1>
+  <main class="prose p-6">
+    <div class="w-full flex flex-col place-items-center mx-auto">
+      <h1>This is a H1</h1>
+      <h2>Here is a H2</h2>
+      <h3>Here is a H3</h3>
+      <p>Here is a paragraph tag</p>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
 //scripts go here
 </script>
-
-<style scoped></style>
